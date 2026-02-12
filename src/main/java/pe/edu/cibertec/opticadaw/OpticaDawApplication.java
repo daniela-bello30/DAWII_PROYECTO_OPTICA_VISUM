223ea6a8a121d44c86e8b4b396833cad82fdd473
danielabellgut-stack <daniela.bellgut@gmail.com>
2026-02-11 23:26:06 -0500

    Primer commit - optica-daw

---
+++ src/main/java/pe/edu/cibertec/opticadaw/OpticaDawApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.opticadaw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpticaDawApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OpticaDawApplication.class, args);
+    }
+
+}
