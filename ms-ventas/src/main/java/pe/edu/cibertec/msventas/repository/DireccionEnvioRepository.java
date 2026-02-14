3ee10f0d0760c9f1bebe935d72d185cd9eae7f4c
luismontanezc <luism.montanezc@gmail.com>
2026-02-14 00:41:01 -0500

    Se creo carpeta repository y sus interfaces

---
+++ ms-ventas/src/main/java/pe/edu/cibertec/msventas/repository/DireccionEnvioRepository.java
@@ -0,0 +1,4 @@
+package pe.edu.cibertec.msventas.repository;
+
+public interface DireccionEnvioRepository {
+}
---
+++ ms-ventas/src/main/java/pe/edu/cibertec/msventas/repository/PedidoRepository.java
@@ -0,0 +1,4 @@
+package pe.edu.cibertec.msventas.repository;
+
+public class PedidoRepository {
+}
